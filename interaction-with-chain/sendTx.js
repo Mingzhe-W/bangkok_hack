8245eca02cdb62e0e9dcd1d39cab6626b5851c3d
Yola <yying001@e.ntu.edu.sg>
2024-11-17 00:09:28 +0700

    fix some bugs

--- interaction-with-chain/sendTx.js
+++ interaction-with-chain/sendTx.js
@@ -1,7 +1,14 @@
-// todo: read from, to, value from python output
-// todo: execute: npx hardhat run sendTx.js --network sepolia in terminal
+const { ethers } = require('hardhat');
+const fs = require('fs');
+
 async function main() {
-    const { ethers } = require('hardhat');
+    // 读取 output.json 文件
+    const data = JSON.parse(fs.readFileSync('output.json', 'utf8'));
+
+    // 验证 JSON 数据完整性
+    if (!data.to || !data.amount) {
+        throw new Error('Missing required fields in output.json: "to" and "amount"');
+    }
 
     // 获取合适的网络 provider
     const provider = new ethers.providers.JsonRpcProvider(process.env.SEPOLIA_URL);
@@ -11,18 +18,20 @@ async function main() {
 
     // 设定交易参数
     const tx = {
-        to: '0xb18E350DFa4063317849Dd0c76cE9c0eB7b2cc5c', // 接收方地址
-        value: ethers.utils.parseEther('0.01'), // 发送的以太数量，0.01ETH
+        to: data.to, // 从 output.json 获取接收方地址
+        value: ethers.utils.parseEther(data.amount.toString()), // 从 output.json 获取发送金额
         gasLimit: 21000, // 固定 gas 上限
     };
 
     // 发起交易
+    console.log('Sending transaction:', tx);
     const transaction = await wallet.sendTransaction(tx);
 
     // 等待交易确认
     const receipt = await transaction.wait();
     console.log('Transaction successful with hash:', receipt.transactionHash);
 }
+
 main()
     .then(() => process.exit(0))
     .catch((error) => {
