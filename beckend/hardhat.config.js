7b0e7db44110918ed0e95b656ae318da66a82999
Mingzhe-W <mingzhe3@illinois.edu>
2024-11-17 09:01:31 +0700

    config

--- beckend/hardhat.config.js
+++ beckend/hardhat.config.js
@@ -5,8 +5,42 @@ module.exports = {
   solidity: "0.8.27",
   networks: {
     sepolia: {
-      url: process.env.SEPOLIA_URL, // 在 .env 文件中配置
+      url: process.env.ALCHEMY_URL, // 在 .env 文件中配置
       accounts: [process.env.PRIVATE_KEY] // 在 .env 文件中配置
-    }
+    },
+    morphl2: {
+      url: 'https://rpc-quicknode-holesky.morphl2.io',
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      gasPrice: 2000000000
+    },
+    scrollSepolia: {
+      url: "https://sepolia-rpc.scroll.io/" || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    polygonpos: {
+      url: process.env.ALCHEMY_URL, // 在 .env 文件中配置
+      accounts: [process.env.PRIVATE_KEY] // 在 .env 文件中配
+    },
+    mantle: {
+      url: process.env.ALCHEMY_URL, // 在 .env 文件中配置
+      accounts: [process.env.PRIVATE_KEY] // 在 .env 文件中配
+    },
+    basesepolia: {
+      url: process.env.ALCHEMY_URL, // 在 .env 文件中配置
+      accounts: [process.env.PRIVATE_KEY] // 在 .env 文件中配
+    },
+    lineasepolia: {
+      url: process.env.ALCHEMY_URL, // 在 .env 文件中配置
+      accounts: [process.env.PRIVATE_KEY] // 在 .env 文件中配
+    },
+    unichainsepolia: {
+      url: process.env.ALCHEMY_URL, // 在 .env 文件中配置
+      accounts: [process.env.PRIVATE_KEY] // 在 .env 文件中配
+    },
+    genosissepolia: {
+      url: process.env.ALCHEMY_URL, // 在 .env 文件中配置
+      accounts: [process.env.PRIVATE_KEY] // 在 .env 文件中配
+    },
   }
 };
